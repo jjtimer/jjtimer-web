3bb4c6d47685099b6bde49637a30a24154009161
Joey Gouly <joey.gouly@gmail.com>
2014-10-27 16:44:58 +0000

    Stats: Refactor the logic for deciding when to show an average.
    This fixes a bug when a session was loaded, and the avg5 or avg12 would
    show 'DNF', because it wasn't being hidden.

--- src/js/Stats.js
+++ src/js/Stats.js
@@ -12,10 +12,6 @@ var Stats = (function(Session) {
     $('session-count').textContent = Session.length();
     if (Session.length() < 3) {
       hide('session-avg-outer');
-      hide('current-avg-all-outer');
-      hide('best-avg-all-outer');
-      hide('current-avg-12-outer');
-      hide('best-avg-12-outer');
     } else {
       show('session-avg-outer');
       $('session-avg').textContent = format_time(Session.average().avg);
@@ -25,12 +21,18 @@ var Stats = (function(Session) {
       $('best-avg-5').textContent = format_time(Session.best_average(5).avg);
       show('current-avg-all-outer');
       show('best-avg-all-outer');
+    } else {
+      hide('current-avg-all-outer');
+      hide('best-avg-all-outer');
     }
     if (Session.length() >= 12) {
       $('current-avg-12').textContent = format_time(Session.current_average(12).avg);
       $('best-avg-12').textContent = format_time(Session.best_average(12).avg);
       show('current-avg-12-outer');
       show('best-avg-12-outer');
+    } else {
+      hide('current-avg-12-outer');
+      hide('best-avg-12-outer');
     }
   }
   return {
